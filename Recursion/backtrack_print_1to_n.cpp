56c71c98e525263c23476b35aa2443224284c927
yaswanth-vempuluru <yaswanthvempuluru@outlook.com>
2024-09-07 00:26:37 +0530

    recursion

---
+++ Recursion/backtrack_print_1to_n.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void print(int i, int n)
+{
+    if (i <= 0)
+    {
+        return;
+    }
+
+    print(i - 1, n);
+    cout << i << endl;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    print(n, n);
+
+    return 0;
+}
